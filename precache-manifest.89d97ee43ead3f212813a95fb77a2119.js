360cde57c161b0af3241e00bef6fdbcbdaf3a529
EnriqueHernandezL <enrique.hl.7@hotmail.com>
2019-12-25 17:57:44 +0100

    Updates

---
+++ precache-manifest.89d97ee43ead3f212813a95fb77a2119.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6bee229d37200f921f143033a76408bc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a821f5d6da4b7e210078",
+    "url": "/static/css/main.7c421f3c.chunk.css"
+  },
+  {
+    "revision": "eeb39329faea4ae50c9f",
+    "url": "/static/js/2.92e07733.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.92e07733.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a821f5d6da4b7e210078",
+    "url": "/static/js/main.c0e26222.chunk.js"
+  },
+  {
+    "revision": "d8312bac4207345d4090",
+    "url": "/static/js/runtime-main.21bdfd9b.js"
+  }
+]);
